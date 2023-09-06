c8d70e07be01debbb00a7206ff3d002a3f76678b
Tomasz Kubicki <80661109+TKtjpk@users.noreply.github.com>
2023-09-06 16:01:25 +0200

    init

--- Sources/SubnetCalculations/SubnetCalculations.swift
+++ Sources/SubnetCalculations/SubnetCalculations.swift
@@ -1,6 +1,95 @@
+import Foundation
+
 public struct SubnetCalculations {
-    public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    // MARK: Public available functions
+    
+    public func returnIPAsBinariesArray(ipAddress: String) -> [String] {
+        var returnValue: [String] = []
+        let array = ipAddress.split(separator: ".")
+        array.forEach { element in
+            returnValue.append(uintToEightBits(number: UInt8(element)!))
+        }
+        return returnValue
+    }
+    
+    public func returnMaskAsArray(mask: Int) -> [UInt8] {
+        var limit = mask
+        var returnValue: [UInt8] = []
+        
+        for _ in 0..<4 {
+            let factor = limit / 8 > 0 ? 8 : limit % 8
+            returnValue.append(factor >= 1 ? uint8fromMaskCIDRNotation(factor: factor, rev: false) : 0)
+            limit -= 8
+        }
+        return returnValue
+    }
+    
+    public func returnMaskAsString(mask: Int) -> String {
+        let array = returnMaskAsArray(mask: mask)
+        return returnCIDRNotationStringFromArray(array: array)
+    }
+    
+    public func returnMaskAsBinariesArray(mask: Int) -> [String] {
+        let array = returnMaskAsArray(mask: mask)
+        var returnValue: [String] = []
+        array.forEach { element in
+            returnValue.append(uintToEightBits(number: element))
+        }
+        return returnValue
+    }
+
+    public func returnWildCardAsArray(mask: Int) -> [UInt8] {
+        var limit = 32 - mask
+        var returnValue: [UInt8] = []
+        
+        for _ in 0..<4 {
+            let factor = limit / 8 > 0 ? 8 : limit % 8
+            returnValue.append(factor >= 1 ? uint8fromMaskCIDRNotation(factor: factor, rev: true) : 0)
+            limit -= 8
+        }
+        return returnValue.reversed()
+    }
+    
+    public func returnWildCardAsString(mask: Int) -> String {
+        let array = returnWildCardAsArray(mask: mask)
+        return returnCIDRNotationStringFromArray(array: array)
+    }
+    
+    public func returnWildCardAsBinariesArray(mask: Int) -> [String] {
+        let array = returnWildCardAsArray(mask: mask)
+        var returnValue: [String] = []
+        array.forEach { element in
+            returnValue.append(uintToEightBits(number: element))
+        }
+        return returnValue
+    }
+    
+    // MARK: Private Functions Only !
+    
+    private func returnCIDRNotationStringFromArray(array: [UInt8]) -> String {
+        var returnValue = ""
+        for i in 0..<4 {
+            returnValue.append(String(array[i]))
+            returnValue.append(i < 3 ? "." : "")
+        }
+        return returnValue
+    }
+    
+    private func uintToEightBits(number: UInt8) -> String {
+        let string = String(number, radix: 2)
+        return repeatElement("0", count: 8 - string.count) + string
+    }
+    
+    private func uint8fromMaskCIDRNotation(factor: Int, rev: Bool) -> UInt8 {
+        let revFactor = rev ? factor : 8
+        var returnValue: UInt8 = 0
+        for i in 1...factor {
+            returnValue += UInt8(pow(2, Double(revFactor - i)))
+        }
+        return returnValue
     }
 }
--- Tests/SubnetCalculationsTests/SubnetCalculationsTests.swift
+++ Tests/SubnetCalculationsTests/SubnetCalculationsTests.swift
@@ -2,10 +2,41 @@ import XCTest
 @testable import SubnetCalculations
 
 final class SubnetCalculationsTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SubnetCalculations().text, "Hello, World!")
+    let maskArr = ["0.0.0.0", "128.0.0.0", "192.0.0.0", "224.0.0.0", "240.0.0.0", "248.0.0.0", "252.0.0.0", "254.0.0.0", "255.0.0.0", "255.128.0.0", "255.192.0.0", "255.224.0.0", "255.240.0.0", "255.248.0.0", "255.252.0.0", "255.254.0.0", "255.255.0.0", "255.255.128.0", "255.255.192.0", "255.255.224.0", "255.255.240.0", "255.255.248.0", "255.255.252.0", "255.255.254.0", "255.255.255.0", "255.255.255.128", "255.255.255.192", "255.255.255.224", "255.255.255.240", "255.255.255.248", "255.255.255.252", "255.255.255.254", "255.255.255.255"]
+    
+    let wildCardArr = ["255.255.255.255", "127.255.255.255", "63.255.255.255", "31.255.255.255", "15.255.255.255", "7.255.255.255", "3.255.255.255", "1.255.255.255", "0.255.255.255", "0.127.255.255", "0.63.255.255", "0.31.255.255", "0.15.255.255", "0.7.255.255", "0.3.255.255", "0.1.255.255", "0.0.255.255", "0.0.127.255", "0.0.63.255", "0.0.31.255", "0.0.15.255", "0.0.7.255", "0.0.3.255", "0.0.1.255", "0.0.0.255", "0.0.0.127", "0.0.0.63", "0.0.0.31", "0.0.0.15", "0.0.0.7", "0.0.0.3", "0.0.0.1", "0.0.0.0"]
+    
+    var binaries: SubnetCalculations? = nil
+
+    override func setUpWithError() throws {
+        binaries = SubnetCalculations()
+    }
+
+    override func tearDownWithError() throws {
+        binaries = nil
+    }
+
+    func test_IPToBinaries() throws {
+        for i in 0...32 {
+            let result = binaries?.returnIPAsBinariesArray(ipAddress: wildCardArr[i])
+            let expected = binaries?.returnWildCardAsBinariesArray(mask: i)
+            XCTAssertEqual(result, expected, "Not Equal")
+        }
+    }
+
+    func test_MaskToString() throws {
+        for i in 0...32 {
+            let result = binaries?.returnMaskAsString(mask: i)
+            let expected = maskArr[i]
+            XCTAssertEqual(result, expected, "Mask doesn't match")
+        }
+    }
+    
+    func test_WildCardToString() throws {
+        for i in 0...32 {
+            let result = binaries?.returnWildCardAsString(mask: i)
+            let expected = wildCardArr[i]
+            XCTAssertEqual(result, expected, "Wild card doesn't match")
+        }
     }
 }
