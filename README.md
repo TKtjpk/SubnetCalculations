2db6477332d00dc89f772cd0bf4ecead07cd1a16
Tomasz Kubicki <80661109+TKtjpk@users.noreply.github.com>
2023-09-06 15:50:16 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SubnetCalculations
+
+A description of this package.
---
+++ Sources/SubnetCalculations/SubnetCalculations.swift
@@ -0,0 +1,6 @@
+public struct SubnetCalculations {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
